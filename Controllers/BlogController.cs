715ac9d1c338a1acbc699582524e5db6da92e53f
Nyasia Williams <ny.williams123@gmail.com>
2020-08-28 12:03:55 -0400

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2_1_AspNetMVC_DailyBlogger.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_1_AspNetMVC_DailyBlogger.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext _context;
+
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult List()
+        {
+            IEnumerable<BlogPost> posts = _context.blogPost.ToList<BlogPost>();
+            return View(posts);
+        }
+
+
+        public IActionResult New()
+        {
+            BlogPost blogPost = new BlogPost();
+            return View(blogPost);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("blogTitle, content, blogDate")] BlogPost blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+
+            // Created but not setup yet.
+            return View(blog);
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+
+        }
+
+        public IActionResult Details(int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([Bind("blogTitle, content, blogDate, id")] BlogPost blog)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                _context.Update(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+
+            }
+
+            return View(blog);
+
+        }
+
+        public IActionResult Delete([Bind("id")] int id)
+        {
+
+            BlogPost blogPost = _context.blogPost.Find(id);
+            _context.Remove(blogPost);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(List));
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
